ca544e4364231bb6a5898d63a03ac05a3c2b3bda
Muhammad Hermas Yuda Pamungkas <hermas.yuda@gmail.com>
2023-09-25 21:33:26 +0700

    without data layer

--- app/src/main/java/com/catnip/incrementdecrementmvvm/MainActivity.kt
+++ app/src/main/java/com/catnip/incrementdecrementmvvm/MainActivity.kt
@@ -2,25 +2,29 @@ package com.catnip.incrementdecrementmvvm
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
 import com.catnip.incrementdecrementmvvm.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-
-    private var counter: Int = 0
-
     private val binding: ActivityMainBinding by lazy {
         ActivityMainBinding.inflate(layoutInflater)
+    }
+
+    private val viewModel: MainViewModel by lazy {
+        ViewModelProvider(this)[MainViewModel::class.java]
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
         setOnClickListener()
-        setInitState()
+        observeState()
     }
 
-    private fun setInitState() {
-        binding.tvCounter.text = counter.toString()
+    private fun observeState() {
+        viewModel.counter.observe(this) {
+            binding.tvCounter.text = it.toString()
+        }
     }
 
     private fun setOnClickListener() {
@@ -33,14 +37,11 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun decrement() {
-        if (counter <= 0) return
-        counter -= 1
-        binding.tvCounter.text = counter.toString()
+        viewModel.decrement()
     }
 
     private fun increment() {
-        counter += 1
-        binding.tvCounter.text = counter.toString()
+        viewModel.increment()
     }
 
 }
